0647f6f8f50ad97607427f023af9fde6fbca15d9
Rash N. Alaswad <132019035+rashaNAlaswad@users.noreply.github.com>
2025-02-05 16:46:12 +0200

    [724] [Time Beats: 39.78%] [Memory Beats: 21.19%] - LeetPush

---
+++ 724-Find-Pivot-Index.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun pivotIndex(nums: IntArray): Int {
+        var totalSum = nums.sum()
+        var leftSum = 0
+        for(i in nums.indices){
+            if(leftSum == totalSum - leftSum - nums[i]){
+                return i
+            }
+            leftSum += nums[i]
+        }
+       return -1 
+    }
+}
