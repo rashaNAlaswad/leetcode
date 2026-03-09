bf56c80928e7f479ac953d1ba4b91545e545476c
Rash N. Alaswad <132019035+rashaNAlaswad@users.noreply.github.com>
2026-03-09 09:00:03 +0200

    [347] [Time Beats: 79.18%] [Memory Beats: 63.30%] - LeetPush

--- Top-K-Frequent-Elements.kt
+++ Top-K-Frequent-Elements.kt
@@ -1,23 +1,25 @@
 1class Solution {
 2    fun topKFrequent(nums: IntArray, k: Int): IntArray {
-3        val map = mutableMapOf<Int,Int>()
-4        for(i in nums){
-5            map[i] = map.getOrDefault(i,0)+1
+3        val map = HashMap<Int,Int>()
+4        for(num in nums){
+5            map[num] = map.getOrDefault(num, 0)+1
 6        }
 7
-8        val buckets = Array<MutableList<Int>>(nums.size + 1) {mutableListOf()}
+8        val bucket = Array(nums.size +1) {mutableListOf<Int>()}
 9        for((num, freq) in map){
-10            buckets[freq].add(num)
+10            bucket[freq].add(num)
 11        }
 12
 13        val result = mutableListOf<Int>()
-14
-15        for(freq in buckets.size-1 downTo 1){
-16            for(num in buckets[freq]){
-17                result.add(num)
-18                if(result.size==k) return result.toIntArray()
-19            }
-20        }
-21        return result.toIntArray()
-22    }
-23}
+14        for(freq in bucket.size - 1 downTo 0){
+15            for(num in bucket[freq]){
+16                result.add(num)
+17            
+18            if(result.size == k){
+19                return result.toIntArray()
+20                }
+21            }
+22        }
+23        return result.toIntArray()
+24    }
+25}
