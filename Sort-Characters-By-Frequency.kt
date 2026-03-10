4998203ed064e364402c3a1172f1986f3fc3d430
Rash N. Alaswad <132019035+rashaNAlaswad@users.noreply.github.com>
2026-03-10 10:29:36 +0200

    [451] [Time Beats: 76.09%] [Memory Beats: 5.43%] - LeetPush

--- Sort-Characters-By-Frequency.kt
+++ Sort-Characters-By-Frequency.kt
@@ -1,21 +1,21 @@
 1class Solution {
 2    fun frequencySort(s: String): String {
-3        val map = mutableMapOf<Char,Int>()
+3        val map = HashMap<Char,Int>()
 4        for(i in s){
 5            map[i] = map.getOrDefault(i,0)+1
 6        }
-7
-8        val buckets = Array<MutableList<Char>>(s.length + 1) { mutableListOf() }
-9        for((char, freq) in map){
+7        val buckets = Array(s.length + 1) { mutableListOf<Char>() }
+8
+9        for ((char, freq) in map) {
 10            buckets[freq].add(char)
 11        }
 12
 13        val result = StringBuilder()
 14        for(freq in buckets.size - 1 downTo 1){
 15            for(char in buckets[freq]){
-16                for(i in 1..freq){
+16                repeat(freq) {
 17                    result.append(char)
-18                }   
+18                }
 19            }
 20        }
 21        return result.toString()
