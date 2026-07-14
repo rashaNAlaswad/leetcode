1c7e179d835f26d45293b73eb66c80dd1e057088
Rasha N. Alaswad <132019035+rashaNAlaswad@users.noreply.github.com>
2026-07-14 15:53:07 +0300

    [915] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 915-Partition-Array-into-Disjoint-Intervals.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun partitionDisjoint(nums: IntArray): Int {
+        // max(Left) <= min(Right)
+        var leftMax = nums[0]
+        var currentMax = nums[0]
+        var partIndex = 0
+
+        for(i in 1 until nums.size){
+            currentMax = max(leftMax, nums[i])
+            if(nums[i] < leftMax){
+                partIndex =i
+                leftMax = currentMax
+            }
+        }
+        return partIndex +1
+    }
+}
